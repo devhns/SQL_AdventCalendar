185676b23405f8ed83066155b919eaa1bfde32d4
Havvanur Selamet <89608627+devhns@users.noreply.github.com>
2024-12-09 02:10:54 +0300

    Add files via upload

---
+++ Day 1.sql
@@ -0,0 +1,3 @@
+select custoselect customer_name,count(distinct(activity)) from rentals
+group by customer_name
+having count(distinct(activity))>1
---
+++ Day 2.sql
@@ -0,0 +1,2 @@
+select gift_name, weight_kg from gifts
+where weight_kg > 1 
